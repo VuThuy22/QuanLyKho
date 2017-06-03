e066bbafda895352f58df061de5253f64604a88c
manhhiep1996 <manhhiep1996@gmail.com>
2017-06-03 17:01:43 +0700

    xong module nhà cung cấp

--- QuanLyKhoHang/QuanLyKhoHang/Controller/NhaCungCapCtl.cs
+++ QuanLyKhoHang/QuanLyKhoHang/Controller/NhaCungCapCtl.cs
@@ -13,8 +13,102 @@ namespace QuanLyKhoHang.Controller
     {
         ConnectToSql con = new ConnectToSql();
         SqlCommand cmd = new SqlCommand();
-        
 
+        public DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+            cmd.CommandText = "select * from NhaCC";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+                con.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return dt;
+        }
+
+        /// <summary>
+        /// Hàm Thêm nhân viên vào danh sách
+        /// </summary>
+        /// <param name="nccobj">đối tượng cần thêm vào ds</param>
+        public bool AddNhaCungCap(NhaCungCapObj nccobj)
+        {
+            cmd.CommandText = "Insert into NhaCC values ('" + nccobj.MaNCC + "','" + nccobj.TenNCC + "','" + nccobj.DiaChi + "','" + nccobj.SDT + "')";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                cmd.ExecuteNonQuery();
+                con.CloseConnection();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Hàm xóa một nhân viên ra khỏi danh sách
+        /// </summary>
+        /// <param name="ma"> mã nhân viên cần xóa</param>
+        public bool DelNhaCungCap(string ma)
+        {
+            cmd.CommandText = "delete NhaCC where MaNCC= '" + ma + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                cmd.ExecuteNonQuery();
+                con.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Hàm sửa thông tin một nhà cung cấp 
+        /// </summary>
+        /// <param name="nccobj"> đối tượng nhà cung cấp cần sửa</param>
+        public bool UpdateNhaCungCap(NhaCungCapObj nccobj)
+        {
+            cmd.CommandText = " update NhaCC set MaNCC='" + nccobj.MaNCC + "',TenNCC='" + nccobj.TenNCC + "',DiaChi='" + nccobj.DiaChi + "',SDT='" + nccobj.SDT + "' where MaNCC='" + nccobj.MaNCC + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                cmd.ExecuteNonQuery();
+                con.CloseConnection();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return true;
+        }
 
     }
 }
